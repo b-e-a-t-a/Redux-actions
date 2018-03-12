a07cf773cec610e8e2ab84149d0e2f275a6dd1a2
Beata Karolczak <karolczak.beata@gmail.com>
2018-03-12 23:01:08 +0100

    Add comment reducer & main reducer

--- reducer.js
+++ reducer.js
@@ -1,49 +1,19 @@
-import {ADD_COMMENT} from './actions';
-import {REMOVE_COMMENT} from './actions';
-import {EDIT_COMMENT} from './actions';
-import {THUMB_UP_COMMENT} from './actions';
-import {THUMB_DOWN_COMMENT} from './actions';
+//main reducer
+import { combineReducers } from 'redux';
+import comments from './comments';
+import users from './users';
 
-const initialState = {
-	comments: [],
-	users: []
-};
+const app = combineReducers({
+	comments,
+	users
+});
 
-function reducer(state = initialState, action) {
-	switch(action.type) {
-		case ADD_COMMENT:
-			return Object.assign({}, state, {
-				comments: [
-				{
-					id: action.id,
-					text: action.text,
-					votes: 0
-				}
-				, ...state]
-			})
-		case REMOVE_COMMENT:
-			return Object.assign({}, state, {
-				comments: state.comments.filter(comment => comment.id !== action.id)
-			});
-		case EDIT_COMMENT:
-			return Object.assign({}, state, {
-				comments: state.comments.map(comment => (comment.id === action.id ?
-					{...comment, text: action.text} : comment)
-				);
-			});
-		case THUMB_UP_COMMENT:
-			return Object.assign({}, state, {
-				comments: state.comments.map(comment => (comment.id === action.id ? 
-					{...comment, votes: comment.votes + 1} : comment)
-				);
-			});
-		case THUMB_DOWN_COMMENT:
-			return Object.assign({}, state, {
-				comments: state.comments.map(comment => (comment.id === action.id ? 
-					{...comment, votes: comment.votes - 1} : comment)
-				);
-			});
-		default:
-			return state;
-	}	
+
+/*
+function app(state = initialState, action) {
+	return {
+		comments: comments(state.comments, action),
+		users: users(state.users, action)
+	};
 }
+*/
